1e2d6643f535e7de3238995e0bdf10160575dcd4
AbdulRaqeebGit <abdulraqeebkumhar@gmail.com>
2024-12-14 16:24:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// // function Counter() {
+
+// //     var outerVal = 0;
+
+// //     function innerFunc() {
+// //         console.log("Count "+outerVal)
+// //         outerVal++
+// //     }
+
+// //     return innerFunc
+// // }
+
+// // const callOuterFunc = Counter();
+
+// // callOuterFunc()
+// // callOuterFunc()
+// // callOuterFunc()
+// // callOuterFunc()
+// // callOuterFunc()
+// // callOuterFunc()
+// // callOuterFunc()
+
+// // var array = {
+// //     name : "AbdulRaqeeb",
+// //     age:16,
+// //     degree: "Matric",
+// //     address : "karachi"
+// // }
+
+// // let {name,age,degree,address} = array;
+// // console.log(name,"\n",age,"\n",degree,"\n",address)
+
+
+// function formFunc() {
+//     var array = ["Abdul Raqeeb","Abdul Razzaq","Matric","16","Male"];
+//     return array
+// }
+
+// let Form = formFunc();
+
+// const[name,SonOf,Coligication,age,Gender] = Form;
+// console.log(name,"\n",SonOf,"\n",Coligication,"\n",age,"\n",Gender);
